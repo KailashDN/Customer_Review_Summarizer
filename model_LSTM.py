9ad61cb1aea4cf57563971b708d100b13f1c165e
Kailash Nadkar <46610479+KailashDN@users.noreply.github.com>
2020-12-08 23:29:19 -0500

    Update model_LSTM.py

--- model_LSTM.py
+++ model_LSTM.py
@@ -8,9 +8,6 @@ from config import *
 def model_LSTM(x_voc, y_voc):
     K.clear_session()
 
-    latent_dim = 300
-    embedding_dim = 100
-
     # Encoder
     encoder_inputs = Input(shape=(max_text_len,))
 
@@ -18,25 +15,25 @@ def model_LSTM(x_voc, y_voc):
     enc_emb = Embedding(x_voc, embedding_dim,trainable=True)(encoder_inputs)
 
     # encoder lstm 1
-    encoder_lstm1 = LSTM(latent_dim,return_sequences=True,return_state=True,dropout=0.4,recurrent_dropout=0.4)
+    encoder_lstm1 = LSTM(latent_dim, return_sequences=True, return_state=True, dropout=0.4, recurrent_dropout=0.4)
     encoder_output1, state_h1, state_c1 = encoder_lstm1(enc_emb)
 
     # encoder lstm 2
-    encoder_lstm2 = LSTM(latent_dim,return_sequences=True,return_state=True,dropout=0.4,recurrent_dropout=0.4)
+    encoder_lstm2 = LSTM(latent_dim, return_sequences=True,return_state=True, dropout=0.4, recurrent_dropout=0.4)
     encoder_output2, state_h2, state_c2 = encoder_lstm2(encoder_output1)
 
     # encoder lstm 3
-    encoder_lstm3 = LSTM(latent_dim, return_state=True, return_sequences=True,dropout=0.4,recurrent_dropout=0.4)
+    encoder_lstm3 = LSTM(latent_dim, return_state=True, return_sequences=True, dropout=0.4, recurrent_dropout=0.4)
     encoder_outputs, state_h, state_c= encoder_lstm3(encoder_output2)
 
     # Set up the decoder, using `encoder_states` as initial state.
     decoder_inputs = Input(shape=(None,))
 
     # embedding layer
-    dec_emb_layer = Embedding(y_voc, embedding_dim,trainable=True)
+    dec_emb_layer = Embedding(y_voc, embedding_dim, trainable=True)
     dec_emb = dec_emb_layer(decoder_inputs)
 
-    decoder_lstm = LSTM(latent_dim, return_sequences=True, return_state=True,dropout=0.4,recurrent_dropout=0.2)
+    decoder_lstm = LSTM(latent_dim, return_sequences=True, return_state=True, dropout=0.4, recurrent_dropout=0.2)
     decoder_outputs,decoder_fwd_state, decoder_back_state = decoder_lstm(dec_emb,initial_state=[state_h, state_c])
 
     # Attention layer
@@ -47,7 +44,7 @@ def model_LSTM(x_voc, y_voc):
     decoder_concat_input = Concatenate(axis=-1, name='concat_layer')([decoder_outputs, attn_out])
 
     # dense layer
-    decoder_dense =  TimeDistributed(Dense(y_voc, activation='softmax'))
+    decoder_dense = TimeDistributed(Dense(y_voc, activation='softmax'))
     decoder_outputs = decoder_dense(decoder_concat_input)
 
     # Define the model
@@ -55,4 +52,34 @@ def model_LSTM(x_voc, y_voc):
 
     model.summary()
 
-    return model
+    """ Creating Inference model: inference for the encoder and decoder:"""
+    # Encode the input sequence to get the feature vector
+    encoder_model = Model(inputs=encoder_inputs, outputs=[encoder_outputs, state_h, state_c])
+
+    # Decoder setup
+    # Below tensors will hold the states of the previous time step
+    decoder_state_input_h = Input(shape=(latent_dim,))
+    decoder_state_input_c = Input(shape=(latent_dim,))
+    decoder_hidden_state_input = Input(shape=(max_text_len, latent_dim))
+
+    # Get the embeddings of the decoder sequence
+    dec_emb2 = dec_emb_layer(decoder_inputs)
+    # To predict the next word in the sequence, set the initial states to the states from the previous time step
+    decoder_outputs2, state_h2, state_c2 = decoder_lstm(dec_emb2,
+                                                        initial_state=[decoder_state_input_h, decoder_state_input_c])
+
+    # attention inference
+    attn_out_inf, attn_states_inf = attn_layer([decoder_hidden_state_input, decoder_outputs2])
+    decoder_inf_concat = Concatenate(axis=-1, name='concat')([decoder_outputs2, attn_out_inf])
+
+    # A dense softmax layer to generate prob dist. over the target vocabulary
+    decoder_outputs2 = decoder_dense(decoder_inf_concat)
+
+    # Final decoder model
+    decoder_model = Model([decoder_inputs] + [decoder_hidden_state_input, decoder_state_input_h, decoder_state_input_c],
+                          [decoder_outputs2] + [state_h2, state_c2])
+
+    encoder_model.save('saved_model_data/encoder_model.h5')
+    decoder_model.save('saved_model_data/decoder_dense.h5')
+
+    return model, encoder_model, decoder_model
